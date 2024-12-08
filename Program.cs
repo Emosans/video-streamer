0458731e3903fd3cba80cdd90087c06e44a4b490
Emosans <shreyasd1414@gmail.com>
2024-12-08 12:52:40 +0530

    :truck: works for conn using IP, error in port forwarding for dif networks

--- Program.cs
+++ Program.cs
@@ -10,20 +10,27 @@ namespace Protocols
         {
             Task.Run(() =>
             {
-                Tcp_server server = new Tcp_server("127.0.0.1", 5000);
+                Tcp_server server = new Tcp_server("", 5000); // specify ip  of your pc
                 server.server();
             });
 
             System.Threading.Thread.Sleep(2000);
+            Console.ReadLine();
 
-            Tcp_client client = new Tcp_client("127.0.0.1", 5000);
-            NetworkStream clientStream = client.Connect();
+            // Tcp_client client = new Tcp_client("127.0.0.1", 5000);
+            // NetworkStream clientStream = client.Connect();
 
-            Video_capture_client video_client = new Video_capture_client(clientStream);
-            video_client.show_captures();
+            // Video_capture_client video_client = new Video_capture_client(clientStream);
+            // video_client.show_captures();
 
-            Console.WriteLine("Press any key to stop...");
-            Console.ReadKey();
+            // Console.WriteLine("Press any key to stop...");
+            // Console.ReadKey();
+
+            // uncomment above code and then comment server code on dif pc and connect to the server.
+
+
+            // works for ip con server - this pc 
+            // client connects to this ip on the same network
 
         }
     }
